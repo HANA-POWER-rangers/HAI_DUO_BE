58b55e784ce5c383bcb9d6ed54a9a3c992037325
Park, seun <seunpark04@gmail.com>
2025-04-30 09:12:14 +0900

    Initial Setting

---
+++ src/main/java/com/kobo/hai_duo/HaiDuoApplication.java
@@ -0,0 +1,13 @@
+package com.kobo.hai_duo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaiDuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaiDuoApplication.class, args);
+    }
+
+}
