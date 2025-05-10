3adeceb36044fdc5ed0839eb4238541f281048d2
Park, seun <seunpark04@gmail.com>
2025-05-10 20:51:50 +0900

    [feat](#7) applyExpAndUpgradeStatus 메서드 GraphQL 조회 : db 미적용

--- src/main/java/com/poweranger/hai_duo/user/api/resolver/UserMutationResolver.java
+++ src/main/java/com/poweranger/hai_duo/user/api/resolver/UserMutationResolver.java
@@ -1,8 +1,10 @@
 package com.poweranger.hai_duo.user.api.resolver;
 
+import com.poweranger.hai_duo.user.api.dto.LevelUpResultDto;
 import com.poweranger.hai_duo.user.application.service.UserService;
 import com.poweranger.hai_duo.user.api.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
@@ -15,6 +17,11 @@ public class UserMutationResolver {
     @MutationMapping
     public UserDto createTempUser() {
         return userService.createTempUser();
+    }
+
+    @MutationMapping
+    public LevelUpResultDto applyExpAndUpgradeStatus(@Argument Long userId, @Argument int amount) {
+        return userService.applyExpAndUpgradeStatus(userId, amount);
     }
 
 }
