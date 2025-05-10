99f56fda2cc50ad659996f58e553bf59b923c1be
Park, seun <seunpark04@gmail.com>
2025-05-10 16:20:23 +0900

    [fix](#5) @Argument 어노테이션 누락으로 인한 userId 전달 오류 수정

--- src/main/java/com/poweranger/hai_duo/user/api/resolver/UserMutationResolver.java
+++ src/main/java/com/poweranger/hai_duo/user/api/resolver/UserMutationResolver.java
@@ -4,6 +4,7 @@ import com.poweranger.hai_duo.user.application.service.UserService;
 import com.poweranger.hai_duo.user.api.dto.UserDto;
 import graphql.kickstart.tools.GraphQLMutationResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +13,7 @@ public class UserMutationResolver implements GraphQLMutationResolver {
 
     private final UserService userService;
 
-    public UserDto getUserById(Long id) {
+    public UserDto getUserById(@Argument("userId") Long id) {
         return userService.getUserById(id);
     }
 }
