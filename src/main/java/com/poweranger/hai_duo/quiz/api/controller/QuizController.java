a98541ce910075cc3069f847f423cdb2fa73e20f
Park, seun <seunpark04@gmail.com>
2025-05-17 15:51:47 +0900

    [fix](#12) 퀴즈 제출 시 퀴즈 정답 유무 포함

--- src/main/java/com/poweranger/hai_duo/quiz/api/controller/QuizController.java
+++ src/main/java/com/poweranger/hai_duo/quiz/api/controller/QuizController.java
@@ -1,8 +1,8 @@
 package com.poweranger.hai_duo.quiz.api.controller;
 
 import com.poweranger.hai_duo.global.response.ApiResponse;
-import com.poweranger.hai_duo.global.response.code.SuccessStatus;
 import com.poweranger.hai_duo.quiz.api.dto.SubmitQuizInputDto;
+import com.poweranger.hai_duo.quiz.api.dto.SubmitQuizResultDto;
 import com.poweranger.hai_duo.quiz.application.service.QuizSubmissionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +18,9 @@ public class QuizController {
     private final QuizSubmissionService quizSubmissionService;
 
     @PostMapping("/submit")
-    public ApiResponse<SuccessStatus> submitQuiz(@RequestBody SubmitQuizInputDto input) {
-        SuccessStatus result = quizSubmissionService.submitQuiz(input);
-        return ApiResponse.onSuccess(result);
+    public ApiResponse<SubmitQuizResultDto> submitQuiz(@RequestBody SubmitQuizInputDto input) {
+        boolean isCorrect = quizSubmissionService.isCorrect(input.stageId(), input.quizType(), input.selectedOption());
+        quizSubmissionService.submitQuiz(input);
+        return ApiResponse.onSuccess(new SubmitQuizResultDto(isCorrect));
     }
 }
--- src/main/java/com/poweranger/hai_duo/quiz/application/service/QuizSubmissionService.java
+++ src/main/java/com/poweranger/hai_duo/quiz/application/service/QuizSubmissionService.java
@@ -1,8 +1,9 @@
 package com.poweranger.hai_duo.quiz.application.service;
 
-import com.poweranger.hai_duo.global.response.code.SuccessStatus;
+import com.poweranger.hai_duo.global.exception.GeneralException;
+import com.poweranger.hai_duo.global.response.code.ErrorStatus;
 import com.poweranger.hai_duo.quiz.api.dto.SubmitQuizInputDto;
-import com.poweranger.hai_duo.quiz.domain.repository.QuizLogRepository;
+import com.poweranger.hai_duo.quiz.domain.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +13,23 @@ import org.springframework.stereotype.Service;
 public class QuizSubmissionService {
 
     private final QuizLogRepository quizLogRepository;
+    private final QuizMeaningRepository quizMeaningRepository;
+    private final QuizCardRepository quizCardRepository;
+    private final QuizOXRepository quizOXRepository;
+    private final QuizBlankRepository quizBlankRepository;
 
-    public SuccessStatus submitQuiz(SubmitQuizInputDto input) {
+    public void submitQuiz(SubmitQuizInputDto input) {
         quizLogRepository.save(input);
-        return SuccessStatus._OK;
+    }
+
+    public boolean isCorrect(Long stageId, String quizType, String selectedOption) {
+        String correctAnswer = switch (quizType) {
+            case "MEAN" -> quizMeaningRepository.findByStage_StageId(stageId).orElseThrow().getWord();
+            case "CARD" -> quizCardRepository.findByStage_StageId(stageId).orElseThrow().getCorrectWord();
+            case "OX" -> String.valueOf(quizOXRepository.findByStage_StageId(stageId).orElseThrow().getWord());
+            case "BLANK" -> quizBlankRepository.findByStage_StageId(stageId).orElseThrow().getCorrectWord();
+            default -> throw new GeneralException(ErrorStatus.QUIZ_TYPE_NOT_FOUND);
+        };
+        return correctAnswer.equals(selectedOption);
     }
 }
