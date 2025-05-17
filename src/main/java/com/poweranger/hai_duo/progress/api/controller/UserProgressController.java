049dacb445fbbd667ffa09fd953a8e7741d32f24
Park, seun <seunpark04@gmail.com>
2025-05-17 16:16:49 +0900

    [refactor](#12) 퀴즈 기록 조회 반환 형식 수정 (apiResponse로 변경)

--- src/main/java/com/poweranger/hai_duo/progress/api/controller/UserProgressController.java
+++ src/main/java/com/poweranger/hai_duo/progress/api/controller/UserProgressController.java
@@ -1,12 +1,12 @@
 package com.poweranger.hai_duo.progress.api.controller;
 
+import com.poweranger.hai_duo.global.response.ApiResponse;
 import com.poweranger.hai_duo.progress.api.dto.ChapterResponseDto;
 import com.poweranger.hai_duo.progress.api.dto.LevelResponseDto;
 import com.poweranger.hai_duo.progress.api.dto.ProgressResponseDto;
 import com.poweranger.hai_duo.progress.api.dto.StageResponseDto;
 import com.poweranger.hai_duo.progress.application.service.UserProgressService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,23 +20,23 @@ public class UserProgressController {
     private final UserProgressService userProgressService;
 
     @GetMapping("/{userId}")
-    public ResponseEntity<ProgressResponseDto> getCurrentProgress(@PathVariable Long userId) {
-        return ResponseEntity.ok(userProgressService.getCurrentProgress(userId));
+    public ApiResponse<ProgressResponseDto> getCurrentProgress(@PathVariable Long userId) {
+        return ApiResponse.onSuccess(userProgressService.getCurrentProgress(userId));
     }
 
     @GetMapping("/{userId}/level")
-    public ResponseEntity<LevelResponseDto> getCurrentLevel(@PathVariable Long userId) {
-        return ResponseEntity.ok(userProgressService.getLatestLevel(userId));
+    public ApiResponse<LevelResponseDto> getCurrentLevel(@PathVariable Long userId) {
+        return ApiResponse.onSuccess(userProgressService.getLatestLevel(userId));
     }
 
     @GetMapping("/{userId}/chapter")
-    public ResponseEntity<ChapterResponseDto> getCurrentChapter(@PathVariable Long userId) {
-        return ResponseEntity.ok(userProgressService.getLatestChapter(userId));
+    public ApiResponse<ChapterResponseDto> getCurrentChapter(@PathVariable Long userId) {
+        return ApiResponse.onSuccess(userProgressService.getLatestChapter(userId));
     }
 
     @GetMapping("/{userId}/stage")
-    public ResponseEntity<StageResponseDto> getCurrentStage(@PathVariable Long userId) {
-        return ResponseEntity.ok(userProgressService.getLatestStage(userId));
+    public ApiResponse<StageResponseDto> getCurrentStage(@PathVariable Long userId) {
+        return ApiResponse.onSuccess(userProgressService.getLatestStage(userId));
     }
 
 }
